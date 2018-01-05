de9bc89efac3d10b75c15b1851a06ef5978d37cb
Freek Paans <freekpaans@gmail.com>
2018-01-05 15:16:45 +0100

    Update not-trusted-constraints.sql

--- not-trusted-constraints.sql
+++ not-trusted-constraints.sql
@@ -1,3 +1,7 @@
+--from https://www.brentozar.com/blitz/foreign-key-trusted/
+--https://blog.sqlauthority.com/2015/01/09/sql-server-what-is-is_not_trusted-in-sys-foreign_keys/
+
+
 SELECT '[' + s.name + '].[' + o.name + '].[' + i.name + ']' AS keyname
 from sys.foreign_keys i
 INNER JOIN sys.objects o ON i.parent_object_id = o.object_id
@@ -11,3 +15,5 @@ INNER JOIN sys.objects o ON i.parent_object_id = o.object_id
 INNER JOIN sys.schemas s ON o.schema_id = s.schema_id
 WHERE i.is_not_trusted = 1 AND i.is_not_for_replication = 0 AND i.is_disabled = 0;
 GO
+
+select is_not_trusted,is_disabled,* from sys.foreign_keys
